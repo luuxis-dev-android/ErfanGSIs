e783a37da23687fc6ba62732a5678571525253d6
luuxis <61792917+luuxis@users.noreply.github.com>
2021-03-21 11:06:44 +0100

    Update gsi.sh

--- gsi.sh
+++ gsi.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-clear
-git clone --recurse-submodules https://github.com/luuxis-dev-android/ErfanGSIs.git
-cd ErfanGSIs
-./setup.sh
 ./url2GSI.sh https://dl.google.com/developers/android/sc/images/factory/flame-spp2.210219.008-factory-404610e1.zip Generic for Android S
